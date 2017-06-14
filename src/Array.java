5adc2713e650d0ace9d640e3afdc175465f357c3
Nitish280 <nsrccs@gmail.com>
2017-06-14 15:49:40 +0530

    arr

---
+++ src/Array.java
@@ -0,0 +1,13 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = { 2,4,5,7,8 };
+		
+	
+		System.out.println(array[5]);
+	}
+
+}
+
